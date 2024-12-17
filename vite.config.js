d470062d1af9cb179bbdb8875293cee86186204e
unknown <satyam1122233@gmail.com>
2024-12-17 10:24:47 +0530

    Added files for build

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/portfolio_website/', // Use the path from the 'homepage' field in your package.json
+  build: {
+    outDir: 'build', // Ensure the build output goes to the 'build' folder
+  },
 })

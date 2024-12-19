938e9812e675f9f006f2c176eb6f9986afcf74f1
unknown <satyam1122233@gmail.com>
2024-12-19 13:49:23 +0530

    Updated files for build

--- src/components/CardView/SkillsCard.jsx
+++ src/components/CardView/SkillsCard.jsx
@@ -3,7 +3,9 @@ import './skills.css';
 function SkillsCard(props) {
   return (
     <div  className='certTxt sm:mx-24 flex flex-col md: gap-4 md:justify-center text-center my-5 md:my-10 md:mx-4 md:w-1/6 '>
-        <div className="skillIC w-full rounded-full  shadow-xl p-12 hover:p-7   cursor-pointer overflow-hidden">
+        <div className="skillIC w-full rounded-full  shadow-xl p-12 hover:p-7   cursor-pointer overflow-hidden
+        
+        ">
         
        
         <props.icon className="reactI w-full h-full "style={{ color: props.iconColor }} />
